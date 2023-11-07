451ad45ca1a2dee9faf13dc4a4477d4d2d90856c
mamadalisilavi <mamadalisilavi@gmail.com>
2023-11-07 10:04:02 +0330

    format prices file

--- src/components/chat/menu-chat/contents/Prices.jsx
+++ src/components/chat/menu-chat/contents/Prices.jsx
@@ -1,12 +1,11 @@
-
 export default function Prices() {
   return (
     <div>
-                    <ul>
-                <li>price 1</li>
-                <li>price 2</li>
-                <li>price 3</li>
-            </ul>
+      <ul>
+        <li>price 1</li>
+        <li>price 2</li>
+        <li>price 3</li>
+      </ul>
     </div>
-  )
+  );
 }
